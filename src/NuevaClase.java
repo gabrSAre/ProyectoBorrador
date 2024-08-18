411fe6e0c12f658aecb317c2da248caf5bed84e8
Gabriel Salgado <gabrsare4593@gmail.com>
2024-08-18 15:34:25 -0500

    Se inserta un for en la rama main

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -1,5 +1,9 @@
 public class NuevaClase {
     public static void main(String[] args) {
         System.out.println("HOLA GIT  v2");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.print(i+" ");
+        }
     }
 }
