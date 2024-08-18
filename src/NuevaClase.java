60fb836ceca81adeba61ea5e7aab312f2554800d
Gabriel Salgado <gabrsare4593@gmail.com>
2024-08-18 15:50:40 -0500

    Modificando mensaje de entrada del login

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -1,6 +1,6 @@
 public class NuevaClase {
     public static void main(String[] args) {
-        System.out.println("HOLA GIT  v2");
+        System.out.println("HOLA GIT  v4 login");
 
         for (int i = 0; i <10 ; i++) {
             System.out.print(i+" ");
