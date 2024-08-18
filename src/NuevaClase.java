e6fca136fe8ac248f1e304ed83ce26a4a755b744
Gabriel Salgado <gabrsare4593@gmail.com>
2024-08-18 16:25:48 -0500

    Se cambian posiciones2

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -4,7 +4,6 @@ public class NuevaClase {
         System.out.println("HOLA GIT  v3");
         System.out.println("Machacacana");
 
-
         for (int i = 0; i <10 ; i++) {
             System.out.print(i+" ");
         }
