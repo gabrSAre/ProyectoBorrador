8cf34c119173ee47361240862920f327084cd6b1
Gabriel Salgado <gabrsare4593@gmail.com>
2024-08-18 16:17:43 -0500

    se generan cambios

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -1,12 +1,12 @@
 public class NuevaClase {
     public static void main(String[] args) {
+        
         System.out.println("HOLA GIT  v3");
+
 
         for (int i = 0; i <10 ; i++) {
             System.out.print(i+" ");
         }
-
         System.out.println("Calabaza");
-
     }
 }
