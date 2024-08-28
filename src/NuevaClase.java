466821583b3180f72caf9edbe5c2492029debc4a
Gabriel Salgado <gabrsare4593@gmail.com>
2024-08-27 21:26:59 -0500

    Se modifica linea de texto

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -1,7 +1,7 @@
 public class NuevaClase {
     public static void main(String[] args) {
 
-        System.out.println("HOLA GIT  v3");
+        System.out.println("HELLO GIT HUB");
         System.out.println("Machacacana");
 
         for (int i = 0; i <10 ; i++) {
