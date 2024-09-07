62bf2abfc6b8bd687735bb08ce4c4e404cf6039e
Gabriel Salgado <gabrsare4593@gmail.com>
2024-09-07 15:29:41 -0500

    Modificación de enunciado, clase Base

--- src/com/githubprueba/gabrsare/Base.java
+++ src/com/githubprueba/gabrsare/Base.java
@@ -2,6 +2,6 @@ package com.githubprueba.gabrsare;
 
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello gitHub 2");
+        System.out.println("Hello gitHub 3");
     }
 }
