7ab157ad0ec37082137a99188d6184583e5861e3
Gabriel Salgado <gabrsare4593@gmail.com>
2024-09-07 15:43:58 -0500

    Se midifica una linea de texto

--- src/com/githubprueba/gabrsare/Base.java
+++ src/com/githubprueba/gabrsare/Base.java
@@ -2,6 +2,6 @@ package com.githubprueba.gabrsare;
 
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello gitHub 3");
+        System.out.println("Hello");
     }
 }
